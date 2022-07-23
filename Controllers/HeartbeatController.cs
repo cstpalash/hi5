8b67a0fe5608facd94751476ede4b5453ca42368
cstpalash <cstpalash@gmail.com>
2022-07-23 18:02:00 +0530

    heartbeat

---
+++ Controllers/HeartbeatController.cs
@@ -0,0 +1,22 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace hi5.bot.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class HeartbeatController : ControllerBase
+    {
+        [HttpGet]
+        [ProducesResponseType(StatusCodes.Status200OK)]
+        public ActionResult<string> Get()
+        {
+            return Ok("OK");
+        }
+    }
+    
+}
